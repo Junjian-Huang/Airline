e5118d3b78dc607382a1c8c35d185f1f5488bc63
Junjian-Huang <junjian.huang@hotmail.com>
2021-08-14 20:12:28 +1200

    Created backend project

---
+++ AirlineBackend/AirlineBackend/Data/AppDbContext .cs
@@ -0,0 +1,16 @@
+﻿using AirlineBackend.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirlineBackend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Aircraft> Aircrafts { get; set; }
+    }
+}
---
+++ AirlineBackend/AirlineBackend/Models/Aircraft.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirlineBackend.Models
+{
+    public class Aircraft
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+
+        [Required]
+        public string GitHub { get; set; }
+
+        public string ImageURL { get; set; }
+    }
+}
