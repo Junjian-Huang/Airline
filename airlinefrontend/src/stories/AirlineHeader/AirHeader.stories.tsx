4666162e4f60444c901c585128234845df85bfdb
Junjian-Huang <junjian.huang@hotmail.com>
2021-08-23 15:27:29 +1200

    set storybook components in file, create side bar

---
+++ airlinefrontend/src/stories/AirlineHeader/AirHeader.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import { Header, HeaderProps } from './AirHeader';
+
+export default {
+  title: 'UI Components/Header',
+  component: Header,
+} as Meta;
+
+const Template: Story<HeaderProps> = (args) => <Header {...args} />;
+
+export const LoggedIn = Template.bind({});
+
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {
+};
